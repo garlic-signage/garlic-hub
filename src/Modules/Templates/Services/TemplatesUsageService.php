b8506cfa46243bc52506fe0781ff08a70eea9fcd
sagiadinos <ns@smil-control.com>
2026-02-23 12:10:55 +0100

    add template usage service

---
+++ src/Modules/Templates/Services/TemplatesUsageService.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Modules\Templates\Services;
+
+use App\Modules\Player\Repositories\PlayerRepository;
+use App\Modules\Playlists\Repositories\ItemsRepository;
+use App\Modules\Templates\Repositories\TemplatesRepository;
+use Doctrine\DBAL\Exception;
+
+// use App\Modules\Playlists\Repositories\ChannelRepository;
+
+readonly class TemplatesUsageService
+{
+    private TemplatesRepository $templatesRepository;
+    private ItemsRepository $itemsRepository;
+    // private readonly ChannelRepository $channelRepository;
+
+    public function __construct(TemplatesRepository $playerRepository, ItemsRepository $itemsRepository /*ChannelRepository $channelRepository*/)
+	{
+        $this->templatesRepository = $playerRepository;
+        $this->itemsRepository  = $itemsRepository;
+        // $this->channelRepository = $channelRepository;
+    }
+
+	/**
+	 * @param int[] $templateIds
+	 * @return array<int,bool>
+	 * @throws Exception
+	 */
+	public function determineTemplatesInUse(array $templateIds): array
+    {
+        $results = [];
+        
+        foreach($this->itemsRepository->countFileResourcesByTemplateId($templateIds) as $value)
+		{
+            $results[$value['templates_id']] = $value['count'];
+        }
+
+        /* no channels currently
+        foreach($this->channelRepository->findTableIdsByPlaylistIds($playlistIds) as $value) {
+            $results[$value['table_id']] = true;
+        }
+        */
+
+        return $results;
+    }
+}
