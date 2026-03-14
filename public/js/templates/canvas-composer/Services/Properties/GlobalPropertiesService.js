a34fe3ae864c085d76e3b9e6b0106d17d2665cc6
sagiadinos <ns@smil-control.com>
2026-03-14 13:07:20 +0100

    enhance GlobalPropertiesService

--- public/js/templates/canvas-composer/Services/Properties/GlobalPropertiesService.js
+++ public/js/templates/canvas-composer/Services/Properties/GlobalPropertiesService.js
@@ -18,72 +18,70 @@
 */
 'use strict';
 
-export class GlobalPropertiesService
+import {BasePropertyService} from "./BasePropertyService.js";
+
+export class GlobalPropertiesService extends BasePropertyService
 {
-	#fabricWrapper;
 
 	constructor(fabricWrapper)
 	{
-		this.#fabricWrapper = fabricWrapper;
+		super(fabricWrapper);
 	}
 
 	getOpacity()
 	{
-		const object = this.#getActiveObject();
+		const object = this._getActiveObject();
 		return object.opacity;
 	}
 
 	setOpacity(value)
 	{
-		const object = this.#getActiveObject();
+		const object = this._getActiveObject();
 		object.set("opacity", Number(value) / 100)
-		this.#fabricWrapper.renderAll();
-		this.#fabricWrapper.fireObjectModified(object);
+		this._updateCanvas();
 	}
 
 	getStrokeColor()
 	{
-		const object = this.#getActiveObject();
+		const object = this._getActiveObject();
 		return object.stroke;
 	}
 
 	setStrokeColor(value)
 	{
-		const object = this.#getActiveObject();
+		const object = this._getActiveObject();
 		object.set("stroke", value);
-		this.#fabricWrapper.renderAll();
-		this.#fabricWrapper.fireObjectModified(object);
+		this._updateCanvas();
 	}
 
 	getStrokeWidth()
 	{
-		const object = this.#getActiveObject();
+		const object = this._getActiveObject();
 		return object.strokeWidth;
 	}
 
 	setStrokeWidth(value)
 	{
-		const object = this.#getActiveObject();
+		const object = this._getActiveObject();
 		object.set("strokeWidth", Number(value));
-		this.#fabricWrapper.renderAll();
-		this.#fabricWrapper.fireObjectModified(object);
+		this._updateCanvas();
 	}
 
 	setPosition(position)
 	{
-		const object = this.#getActiveObject();
+		const object = this._getActiveObject();
 
 		let canvasBound = {
-			width: this.#fabricWrapper.getWidth(),
-			height: this.#fabricWrapper.getHeight(),
+			width: this.fabricWrapper.getWidth(),
+			height: this.fabricWrapper.getHeight(),
 			center: {
-				x: this.#fabricWrapper.getWidth() / 2,
-				y: this.#fabricWrapper.getHeight() / 2
+				x: this.fabricWrapper.getWidth() / 2,
+				y: this.fabricWrapper.getHeight() / 2
 			}
 		}
 
-		let s = object.getBoundingRect().height / this.#fabricWrapper.getViewportTransform()[3],
-			a = object.getBoundingRect().width / this.#fabricWrapper.getViewportTransform()[0];
+		let s = object.getBoundingRect().height / this.fabricWrapper.getViewportTransform()[3],
+			a = object.getBoundingRect().width / this.fabricWrapper.getViewportTransform()[0];
 
 		switch (position)
 		{
@@ -111,14 +109,7 @@ export class GlobalPropertiesService
 			case "right":
 				object.setPositionByOrigin(new fabric.Point(canvasBound.center.x + canvasBound.width / 2 - a / 2, object.getCenterPoint().y), "center", "center")
 		}
-		this.#fabricWrapper.renderAll();
-		this.#fabricWrapper.fireObjectModified(object);
+		this._updateCanvas();
 	}
 
-	#getActiveObject()
-	{
-		const object = this.#fabricWrapper.getActiveObject();
-		if (!object) throw new Error("No active object");
-		return object;
-	}
 }
