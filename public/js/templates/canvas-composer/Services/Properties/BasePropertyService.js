97ff47cee3bf1d8931056bcf71ffe79948ff346f
sagiadinos <ns@smil-control.com>
2026-03-14 13:06:43 +0100

    Add a BasePropertyService

---
+++ public/js/templates/canvas-composer/Services/Properties/BasePropertyService.js
@@ -0,0 +1,42 @@
+/*
+ garlic-hub: Digital Signage Management Platform
+
+ Copyright (C) 2026 Nikolaos Sagiadinos <garlic@saghiadinos.de>
+ This file is part of the garlic-hub source code
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License, version 3,
+ as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+'use strict';
+
+export class BasePropertyService
+{
+	#fabricWrapper;
+
+	constructor(fabricWrapper)
+	{
+		this.#fabricWrapper = fabricWrapper;
+	}
+
+	get fabricWrapper()
+	{
+		return this.#fabricWrapper;
+	}
+
+	_getActiveObject()
+	{
+		const object = this.#fabricWrapper.getActiveObject();
+		if (!object) throw new Error("No active object");
+		return object;
+	}
+
+}
