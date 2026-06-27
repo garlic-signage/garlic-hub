6d32e53a54f988ee9f0a6a2b3e0d76526deb3a53
sagiadinos <ns@smil-control.com>
2026-06-27 18:24:47 +0200

    update autocomplete renderer test

--- tests/Unit/Framework/Utils/Html/AutocompleteRendererTest.php
+++ tests/Unit/Framework/Utils/Html/AutocompleteRendererTest.php
@@ -45,8 +45,8 @@ class AutocompleteRendererTest extends TestCase
 		$fieldMock->method('getValue')->willReturn('123');
 		$fieldMock->method('getDataLabel')->willReturn('Test Label');
 
-		$expectedHtml = '<input id="test-field_search" list="test-field_suggestions" value="Test Label" data-id="123" aria-describedby="error_test-field">
-		<input type="hidden" id="test-field" name="test-field" value="123" autocomplete="off">
+		$expectedHtml = '<input id="test-field_search" list="test-field_suggestions" value="Test Label" data-id="123" aria-describedby="error_test-field" class="autocomplete" autocomplete="off">
+		<input type="hidden" id="test-field" name="test-field" value="123">
 		<datalist id = "test-field_suggestions" ></datalist>';
 
 		$result = $this->renderer->render($fieldMock);
